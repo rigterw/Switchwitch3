18de9838f50a8c76c43fc13ef62499a6d0ebbb1d
Wessel <rigterwessel@gmail.com>
2022-10-04 13:18:39 +0200

    fixed background

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,32 +1,83 @@
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //variables used to spawn backgrounds
+    [SerializeField]
+    List<GameObject> backgrounds;
     public GameObject preFabBG;//the background that needs to be spawned
+    float nextSpawnValue;//the value for when the camera needs to spawn a new background
+    Vector2 imgSize;
 
-    Transform position;
-    Transform playerPos;
-    float nextSpawnValue;
-    float speed;
-    // Start is called before the first frame update
+
+    //variables for the camera speed calculation
+    Transform position;//position of the camera
+    Transform playerPos;//position of the player the camera has to follow
+    Rigidbody2D Rbody;//rigidBody of the player the camera has to follow
+    float playerDirection;//value of the player direction (-1 for left, 1 for right)
+
     void Start()
     {
         position = GetComponent<Transform>();
-        playerPos = GameObject.Find("player").GetComponent<Transform>();
+        GameObject player = GameObject.Find("Player");
+        playerPos = player.GetComponent<Transform>();
+        Rbody = player.GetComponent<Rigidbody2D>();
+
+        backgrounds = new List<GameObject>();
+        GameObject firstBG = GameObject.Find("background");
+        backgrounds.Add(firstBG);
+
+        Transform Imgtransform = firstBG.transform.GetChild(0);
+        SpriteRenderer imgrender = Imgtransform.GetComponent<SpriteRenderer>();
+        imgSize = imgrender.bounds.size;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        Vector2 pos = position.position;
-        if(pos.x > position.position.x){
-            pos.x++;
+        HandleBackGround();
+    }
+
+    void FixedUpdate(){
+        MoveCamera();
+
+    }
+
+
+    /// <summary>
+    /// Function that lets the camera follow the player
+    /// </summary>
+    void MoveCamera(){
+        //updates the direction upon movement
+        if(Rbody.velocity.x != 0.0)
+            playerDirection = Rbody.velocity.x < 0 ? -1 : 1;
+
+        //calculates the camera speed
+        Vector3 power = (playerPos.position + new Vector3(5,0,0)*playerDirection - position.position) / 80;
+
+        //moves the camera
+        position.position += new Vector3(power.x, 0,0);
+        if(position.position.x < 0){
+            position.position = new Vector3(0, position.position.y, position.position.z);
         }
+    }
 
+    void HandleBackGround(){
+        ExtendBackGround();
+    }
 
+    void ExtendBackGround(){
+    if(position.position.x < nextSpawnValue)
+        return;
+    
+    nextSpawnValue += imgSize.x - 0.1f;
+    GameObject lastBG = backgrounds.Last();
+    Vector3 lastBGPos = lastBG.GetComponent<Transform>().position;
+    Instantiate(preFabBG, new Vector3(lastBGPos.x + nextSpawnValue, lastBGPos.y, 0), Quaternion.identity);
+    }
 
         
-    }
 }
+
