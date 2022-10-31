ea2224184d6ac7b3a98a892c3ac4ef633f7f0238
rigterw <rigterwessel@gmail.com>
2022-10-31 20:13:05 +0100

    adds bounce to the enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,10 +5,14 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     float movementSpeed;
     public bool bounce;
-    bool moving = false;
+    bool isFacingRight = true;
     Vector2 velocity;
     Rigidbody2D rb;
-    
+
+    [SerializeField]
+    Transform bouncePoint;
+    float bounceCheckDistance = 0.15f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +22,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rb.velocity.x != 0f)
-        {
-            GetComponent<SpriteRenderer>().flipX = rb.velocity.x <= -0.5f;
-        }
+
+        if (rb.velocity.x > 0.1f || rb.velocity.x < -0.1f)
+            isFacingRight = rb.velocity.x > 0f;
+        
+        if(bounce)
+            Bounce();
+
+        //sets the sprite direction according to the velocity
+        GetComponent<SpriteRenderer>().flipX = !isFacingRight;
     }
 
     void FixedUpdate()
@@ -29,6 +38,36 @@ public class Enemy : MonoBehaviour
         velocity = rb.velocity;
         velocity.x = movementSpeed;
         rb.velocity = velocity;
-        moving = true;
     }
+
+
+    /// <summary>
+    /// function that changes the movement direction when hitting a wall
+    /// </summary>
+    void Bounce(){
+        if (bouncePoint == null) { 
+            Debug.LogError("No bouncePoint given"); 
+            return; 
+        }
+
+        if(BounceCheck()){
+            movementSpeed = -movementSpeed;
+        }
+    }
+
+
+    /// <summary>
+    /// function that checks if enemy is hitting a wall
+    /// </summary>
+    /// <returns>true if the player hits a wall</returns>
+    bool BounceCheck(){
+        Vector2 direction = isFacingRight ? Vector2.right : Vector2.left;
+        RaycastHit2D wallHit = Physics2D.Raycast(bouncePoint.position, direction, bounceCheckDistance);
+        bool shouldBounce = wallHit.collider != null;
+        
+        if(shouldBounce)
+            bouncePoint.localPosition = new Vector2(-bouncePoint.localPosition.x, bouncePoint.localPosition.y);
+        return shouldBounce;
+    }
+
 }
